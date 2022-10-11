9dff353f5f70b04da0c72056ee1b74ba19706e96
wrainbow <wrainbow@deloitte.co.uk>
2022-10-11 17:16:45 +0100

    val loss added to model output

--- src/train.py
+++ src/train.py
@@ -30,7 +30,7 @@ def main(cloud : int,
 
     num_classes = 1
 
-    learning_rates = [2.5e-06, 1e-4, 1e-4, 1e-4, 1e-4, 2.5e-05] #[2.5e-06, 0.000625, 0.00125, 0.0025, 0.00025, 2.5e-05]
+    learning_rates = [1e-4, 5e-4, 5e-4, 1e-4, 1e-4, 2.5e-05] #[2.5e-06, 0.000625, 0.00125, 0.0025, 0.00025, 2.5e-05]
     learning_rate_boundaries = [125, 250, 500, 240000, 360000]
     learning_rate_fn = tf.optimizers.schedules.PiecewiseConstantDecay(
         boundaries=learning_rate_boundaries, values=learning_rates
